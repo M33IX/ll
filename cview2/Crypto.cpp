a929221f21240247a417c1ac8ff28b69ba0d3331
Andrew <zhutavlyov@gmail.com>
2023-12-29 16:22:03 +0300

    ref(Crypto): сделать методы криптографии нестатическими, добавить сеттеры для ключей

--- cview2/Crypto.cpp
+++ cview2/Crypto.cpp
@@ -1,27 +1,72 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+
 #include <cryptopp/files.h> 
 #include <cryptopp/aes.h>
 #include <cryptopp/modes.h>
 #include <cryptopp/filters.h>
 
+constexpr auto LENGTH = CryptoPP::AES::BLOCKSIZE;
+
 using namespace CryptoPP;
 
 class Crypto {
 private:
-	std::string key = "";
-	std::string iv = "";
-public:
-	Crypto() {};
-	~Crypto() {}
-	void SetKey(std::string& key) {
-		this->key = key;
-	}
-	void SetIV(std::string& iv) {
-		this->iv = iv;
-	}
-	void Crypt() {}
-	void Decrypt() {}
-	std::string MakeHash() {}
+    byte key[AES::DEFAULT_KEYLENGTH] = { '0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0' };
+    byte iv[AES::BLOCKSIZE] = { '0','0','0','0','0','0','0','0','0','0','0','0','0','0','0','0' };
+    std::string inputFile;
+    std::string output = "bin";
+
+public: 
+    Crypto(std::string filename) {
+        this->inputFile = filename;
+    }
+    void encryptFile() {
+    std::ifstream ifs(inputFile, std::ios::binary);
+    std::ofstream ofs(output, std::ios::binary);
+
+    AES::Encryption aesEncryption(key, AES::DEFAULT_KEYLENGTH);
+    CBC_Mode_ExternalCipher::Encryption cbcEncryption(aesEncryption, iv);
+
+    FileSource fileSource(ifs, true,
+        new StreamTransformationFilter(cbcEncryption,
+            new FileSink(ofs)
+        )
+    );
+    ifs.close();
+    ofs.close();
+    std::remove(inputFile.c_str());
+    }
+
+    void decryptFile() {
+        std::ifstream ifs(inputFile, std::ios::binary);
+        std::ofstream ofs(output, std::ios::binary);
+
+        AES::Decryption aesDecryption(key, AES::DEFAULT_KEYLENGTH);
+        CBC_Mode_ExternalCipher::Decryption cbcDecryption(aesDecryption, iv);
+
+        FileSource fileSource(ifs, true,
+            new StreamTransformationFilter(cbcDecryption,
+            new FileSink(ofs)
+            )
+        );
+        ifs.close();
+        ofs.close();
+        std::remove(output.c_str());
+      }
+
+      void SetKey(std::string& key) {
+          if (strnlen_s(key.c_str(), 64) != AES::DEFAULT_KEYLENGTH) throw "Wrong key size";
+          for (int i = 0; i < LENGTH; i++) {
+              this->key[i] = key[i];
+          }
+      }
+      void SetIV(std::string& iv) {
+          if (strnlen_s(iv.c_str(), 64) != AES::BLOCKSIZE) throw "Wrong key size";
+          for (int i = 0; i < LENGTH; i++) {
+              this->iv[i] = iv[i];
+          }
+      }
+
 };
